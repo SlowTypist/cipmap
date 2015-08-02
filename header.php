c429e376b6dd402399b68d1826b2f589dfea9b17
Vahe Hakobyan <gfoxvh@gmail.com>
2015-08-03 01:08:15 +0400

    custom title

--- header.php
+++ header.php
@@ -5,3 +5,11 @@
  * Date: 8/3/15
  * Time: 12:40 AM
  */
+
+require_once '/home/cryptexis/dev/vendor/autoload.php';
+
+$loader = new Twig_Loader_Filesystem('templates');
+$twig = new Twig_Environment($loader, array(
+    'cache' => false,
+    'debug' => true
+));
--- login.php
+++ login.php
@@ -1,7 +1,12 @@
 <?php
+require_once("./header.php");
 require_once('config/config.php');
+
+$template = $twig->loadTemplate('index.html');
+echo $template->render(array('title' => 'Login page', 'go' => 'here'));
+
 require_once('includes/user.php');
 include('handlers/login_handler.php');
-include("views/login_view.php");
+//include("views/login_view.php");
 ?> 
 
