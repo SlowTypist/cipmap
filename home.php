83e0db01c6b09ff807752aa0a954162a4a4fe5e7
Vahe Hakobyan <gfoxvh@gmail.com>
2015-08-19 16:40:10 +0400

    home page

--- home.php
+++ home.php
@@ -3,8 +3,11 @@ require_once("./includes/refresh_session.php");
 require_once("./header.php");
 require_once("./controller/LectureController.php");
 
+$template_variables = array('title' => 'Home page');
 $lectureController = new LectureController();
 $allLectures = $lectureController->listAll();
+$template_variables["lectures"] = $allLectures;
 
-include("views/student1_view.php");
+$template = $twig->loadTemplate('app/home.html');
+echo $template->render($template_variables);
 ?> 
