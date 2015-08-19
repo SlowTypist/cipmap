6d035b2eaa7260d355017a06ac2df423d69dcdd9
Vahe Hakobyan <gfoxvh@gmail.com>
2015-08-19 17:07:48 +0400

    password change

--- pwchange.php
+++ pwchange.php
@@ -3,10 +3,14 @@ require_once("./includes/refresh_session.php");
 require_once("./header.php");
 require_once("./controller/UserController.php");
 
+$template_variables = array('title' => 'Change Password');
 if($_POST["action"] == "Change password") {
     $userController = new UserController();
     $result = $userController->changePassword();
+    $template_variables["type"] = $result["type"];
+    $template_variables["message"] = $result["message"];
 }
 
-include("views/pwchange_view.php");
+$template = $twig->loadTemplate('auth/pwchange.html');
+echo $template->render($template_variables);
 ?> 
